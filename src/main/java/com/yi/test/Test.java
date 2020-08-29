29e3f472bb64fc866d83c34d6d5204f49abaafe4
ysy-ai <yh4250503.com>
2020-08-29 10:34:10 +0800

    分支测试

---
+++ src/main/java/com/yi/test/Test.java
@@ -0,0 +1,14 @@
+package com.yi.test;
+
+/**
+ * @Package: com.yi.test
+ * @ClassName: Test
+ * @Author: ThinkPad
+ * @CreateTime: 2020/8/29 10:32
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("分支测试");
+    }
+}
